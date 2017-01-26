984d5222278ca0693d3fda2acfe74dc43ae5877a
yutse <ncu992201010@gmail.com>
2017-01-26 22:46:49 +0800

    firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tony 美髮事業網站';
+  items$: FirebaseListObservable<any[]>;
+  item$: FirebaseObjectObservable<any>;
+
+  constructor(af: AngularFire) {
+   this.items$ =  af.database.list('tonyitems');
+   this.item$ =  af.database.object('tonyitems/ttt');
+  }
+
+  ngOnInit() {
+    let data = this.items$.push({
+      name: 'tony',
+      descript: 'Hello World'
+    });
+
+    let key = data.key;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+
+  var config = {
+    apiKey: "AIzaSyBREZj86wtKfbHpM2pC9wPouKxa9bB8a0U",
+    authDomain: "fir-1-ab77c.firebaseapp.com",
+    databaseURL: "https://fir-1-ab77c.firebaseio.com",
+    storageBucket: "fir-1-ab77c.appspot.com",
+    messagingSenderId: "353613024655"
+  };
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
