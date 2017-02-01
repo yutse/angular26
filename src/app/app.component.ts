499bf4d330bd5d1412f1b0fcc07356ab0416d7f4
yutse <ncu992201010@gmail.com>
2017-02-02 00:19:13 +0800

    init site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,18 @@ import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'a
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Tony 美髮事業網站';
+  title = 'Tony 美髮事業網站(預約功能)';
   items$: FirebaseListObservable<any[]>;
   item$: FirebaseObjectObservable<any>;
+  public timeLength: number = 17;
+  public timearr: string[] = [];
+  public favoriteSeason: string = '染髮(1.5H)';
+  public typeArr: string[] = ['剪髮(1H)', '染髮(1.5H)', '燙髮(2H)'];
+
 
   constructor(af: AngularFire) {
-   this.items$ =  af.database.list('tonyitems');
-   this.item$ =  af.database.object('tonyitems/ttt');
+    this.items$ = af.database.list('tonyitems');
+    this.item$ = af.database.object('tonyitems/ttt');
   }
 
   ngOnInit() {
@@ -23,6 +28,55 @@ export class AppComponent implements OnInit {
     // });
 
     // let key = data.key;
+    this.timearr = this.YCoor();
   }
+
+  /**
+   * 星期轉換
+   */
+  public WeekToWord(J: number): string {
+    const str = '星期';
+    const word = ['一', '二', '三', '四', '五', '六', '天'];
+    return str + word[J];
+  }
+
+  /**
+   * 時間軸
+   */
+  public YCoor(): string[] {
+    let arr: string[] = [];
+    for (let i = 0; i < this.timeLength; i++) {
+      let date = new Date(2017, 0, 1, 8, 30 * i);
+      let date1 = new Date(2017, 0, 1, 8, 30 * (i + 1));
+      arr.push(`${date.getHours()}點${date.getMinutes()}分 至 ${date1.getHours()}點${date1.getMinutes()}分`);
+    }
+    return arr;
+  }
+
+  /**
+   * selectCheck
+   */
+  public selectCheck(I: number, J: number, clickI: number, clickJ: number): string {
+    //  ['剪髮(1H)', '染髮(1.5H)', '燙髮(2H)']
+    let index = 0;
+    switch (this.favoriteSeason) {
+      case '剪髮(1H)':
+        index = 2;
+        break;
+      case '染髮(1.5H)':
+        index = 3;
+        break;
+      case '燙髮(2H)':
+        index = 4;
+        break;
+    }
+    let check = I === clickI;
+    for (let i = 1; i < index; i++) {
+      check = check || I === (clickI + i);
+    }
+    return check && J === clickJ ? 'red' : 'black';
+  }
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
+// import { DatepickerModule } from 'ng2-bootstrap';
+import { MaterialModule } from '@angular/material';
+import { MdRadioModule } from '@angular/material/radio';
+import 'hammerjs';
 
 
-  var config = {
-    apiKey: "AIzaSyBmaW_vl8iS6dunx2UhhCdvoDmtVRatqW8",
-    authDomain: "tonyfair-3b314.firebaseapp.com",
-    databaseURL: "https://tonyfair-3b314.firebaseio.com",
-    storageBucket: "tonyfair-3b314.appspot.com",
-    messagingSenderId: "134616636706"
+  const config = {
+    apiKey: 'AIzaSyBmaW_vl8iS6dunx2UhhCdvoDmtVRatqW8',
+    authDomain: 'tonyfair-3b314.firebaseapp.com',
+    databaseURL: 'https://tonyfair-3b314.firebaseio.com',
+    storageBucket: 'tonyfair-3b314.appspot.com',
+    messagingSenderId: '134616636706'
   };
 
 @NgModule({
@@ -23,7 +27,9 @@ import { AngularFireModule } from 'angularfire2';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(config)
+    AngularFireModule.initializeApp(config),
+    MaterialModule.forRoot()
+    // DatepickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
